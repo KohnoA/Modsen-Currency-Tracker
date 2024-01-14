79fbd9e92572fc4d5bd0e50faa68e231d2908a6a
KohnoA <san.kohno@yandex.ru>
2024-01-14 13:08:15 +0300

    feat: added global styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
