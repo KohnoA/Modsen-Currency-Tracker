8da639eb0d484863adbadfc165fa3da6156f65fc
KohnoA <san.kohno@yandex.ru>
2024-01-24 14:43:15 +0300

    feat: writed tests for filterBanksByCurrenies function

---
+++ src/utils/tests/filterBanksByCurrencies.test.ts
@@ -0,0 +1,16 @@
+import { filterBanksByCurrencies } from '../filterBanksByCurrencies';
+import { BANKS_DATA } from '@/db/banksData';
+
+describe('Testing filterBanksByCurrencies function', () => {
+  it('Should return relevant banks', () => {
+    expect(filterBanksByCurrencies('USD', BANKS_DATA)).toEqual([BANKS_DATA[0], BANKS_DATA[2]]);
+  });
+
+  it('Should not be case sensitive', () => {
+    expect(filterBanksByCurrencies('usd', BANKS_DATA)).toEqual([BANKS_DATA[0], BANKS_DATA[2]]);
+  });
+
+  it('If there are no banks for the specified currency, return an empty array', () => {
+    expect(filterBanksByCurrencies('kmwfewfnw', BANKS_DATA)).toEqual([]);
+  });
+});
