8aae8c9b714a6b45d4ece40dcbd40aa0ef47fe36
KohnoA <san.kohno@yandex.ru>
2024-01-29 11:14:24 +0300

    refactor: splitting webpack configuration

---
+++ webpack.dev.ts
@@ -0,0 +1,57 @@
+import { merge } from 'webpack-merge';
+import { Configuration } from 'webpack';
+import common from './webpack.common';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import 'webpack-dev-server';
+
+export default merge<Configuration>(common, {
+  mode: 'development',
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+        runtimeErrors: true,
+      },
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: ['react-refresh/babel'].filter(Boolean),
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                auto: true,
+                localIdentName: '[name]__[local]',
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+
+  plugins: [new ReactRefreshWebpackPlugin({ overlay: false })],
+});
