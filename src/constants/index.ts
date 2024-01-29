ac7ab09e1ebad107973dde381560f05329c97df0
KohnoA <san.kohno@yandex.ru>
2024-01-29 12:39:14 +0300

    refactor: environment variables

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,12 +1,10 @@
-/* eslint prefer-destructuring: "off" */
-
 export const CURRENCY_MODAL_QUERY_KEY = 'currency';
 
-export const MODSEN_LINK = 'https://www.modsen-software.com/';
-export const AUTHOR_GITHUB = 'https://github.com/KohnoA';
-
-export const CURRENCY_API_BASE = 'https://api.currencyapi.com/v3';
-export const CURRENCY_API_KEY = process.env.CURRENCY_API_KEY;
-
-export const COIN_API_BASE = 'https://rest.coinapi.io/v1/ohlcv';
-export const COIN_API_KEY = process.env.COIN_API_KEY;
+export const {
+  CURRENCY_API_KEY,
+  CURRENCY_API_BASE,
+  COIN_API_KEY,
+  COIN_API_BASE,
+  AUTOR_GITHUB,
+  MODSEN_LINK,
+} = process.env;
