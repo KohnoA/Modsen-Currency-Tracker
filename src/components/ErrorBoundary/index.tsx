07fb983e4ac21fda0fd26b25b2c86d3df749187d
KohnoA <san.kohno@yandex.ru>
2024-01-29 16:57:18 +0300

    refactor: ErrorBoundary component

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -2,7 +2,7 @@ import { Component, ErrorInfo, ReactNode } from 'react';
 
 import { ErrorMessage, Modal } from '../UI';
 
-import styles from './ErrorBoundary.module.scss';
+import styles from './styles.module.scss';
 
 type ErrorBoundaryProps = {
   children: ReactNode;
@@ -12,7 +12,7 @@ type ErrorBoundaryState = {
   hasError: boolean;
 };
 
-export default class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   constructor(props: ErrorBoundaryProps) {
     super(props);
     this.state = { hasError: false };
@@ -23,7 +23,6 @@ export default class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBo
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    // eslint-disable-next-line no-console
     console.error(error, errorInfo);
   }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-import ErrorBoundary from './components/ErrorBoundary';
+import { ErrorBoundary } from './components/ErrorBoundary';
 import App from './App';
 import { store } from './store';
 
