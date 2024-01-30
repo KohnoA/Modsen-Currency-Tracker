c1dd7b2a37581fb7544930630b6bd73f1adb6131
KohnoA <san.kohno@yandex.ru>
2024-01-30 22:21:58 +0300

    feat: added dynamic year update in footer

--- src/layout/Footer/config.ts
+++ src/layout/Footer/config.ts
@@ -1,2 +1,4 @@
+const CURRENT_YEAR = new Date().getFullYear();
+
 export const companyDescription = `Since then, the company has grown organically to. Starsup is the world's largest trading platform, with $12 billion worth of currency trading and 500,000 tickets sold daily to tens of thousands of traders worldwide.`;
-export const copyrightDescription = 'Startsup © 2023-2024, All Rights Reserved';
+export const copyrightDescription = `Startsup © ${CURRENT_YEAR}, All Rights Reserved`;
