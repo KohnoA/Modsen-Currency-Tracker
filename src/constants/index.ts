72b400f3a9c76369ad419c8f507bf36f98787346
KohnoA <san.kohno@yandex.ru>
2024-02-01 23:15:48 +0300

    refactor: constants folder

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,6 @@ export { BANKS_DATA } from './banksData';
 export { CONTACTS_INFO } from './contactsInfo';
 export { DEFAULT_CHART_DATA } from './defaultChartData';
 export { DEFAULT_CURRENCIES } from './defaultCurrencies';
-export { DEFAULT_OHLC_PAIRS } from './defaultOhlcPairs';
 export { FOOTER_LINK_GROUPS } from './footerLinkGroups';
 export { ICONS } from './icons';
 export { PAGE_LIST } from './pageList';
@@ -10,11 +9,4 @@ export { SOCIAL_LIST } from './socialList';
 
 export const CURRENCY_MODAL_QUERY_KEY = 'currency';
 
-export const {
-  CURRENCY_API_KEY,
-  CURRENCY_API_BASE,
-  COIN_API_KEY,
-  COIN_API_BASE,
-  AUTHOR_GITHUB,
-  MODSEN_LINK,
-} = process.env;
+export const { CURRENCY_API_KEY, CURRENCY_API_BASE, AUTHOR_GITHUB, MODSEN_LINK } = process.env;
