a451415233ed5220d686bce380ef344da50b591f
KohnoA <san.kohno@yandex.ru>
2024-02-01 23:11:21 +0300

    refactor: areArraysEqual function

--- src/utils/areArraysEqual.ts
+++ src/utils/areArraysEqual.ts
@@ -1,6 +1,3 @@
 export function areArraysEqual<T>(arr1: T[], arr2: T[]) {
-  const arr1Sorted = [...arr1].sort();
-  const arr2Sorted = [...arr2].sort();
-
-  return arr1Sorted.toString() === arr2Sorted.toString();
+  return arr1.toString() === arr2.toString();
 }
