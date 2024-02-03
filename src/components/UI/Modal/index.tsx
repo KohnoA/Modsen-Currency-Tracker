4d543452bd8e4bdcaaff19f395393cde85ba08b7
KohnoA <san.kohno@yandex.ru>
2024-02-03 19:54:35 +0300

    refactor: removed the comment disabling eslint in the Modal component

--- src/components/UI/Modal/index.tsx
+++ src/components/UI/Modal/index.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */
-
 import { KeyboardEvent, MouseEvent, ReactNode } from 'react';
 import { createPortal } from 'react-dom';
 
